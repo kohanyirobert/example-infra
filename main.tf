d68f2b352ab5c8ddf7eb860d87dc2470d5df4a3d
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-11-22 10:49:52 +0100

    Ignore .tfbackend, run fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.76.0"
-      configuration_aliases = [ aws.eu ]
+      source                = "hashicorp/aws"
+      version               = "5.76.0"
+      configuration_aliases = [aws.eu]
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
