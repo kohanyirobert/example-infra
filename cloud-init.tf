9b1eb6131b6f4b59a02552f2b0e37a39dfddc4a5
Kohányi Róbert <kohanyi.robert@gmail.com>
2024-11-22 13:56:46 +0100

    Run terraform fmt

--- cloud-init.tf
+++ cloud-init.tf
@@ -56,7 +56,7 @@ data "cloudinit_config" "web" {
     content_type = "text/x-shellscript"
     content = templatefile("./scripts/deploy-web.sh.tftpl", {
       api_lb_dns_name = aws_lb.api.dns_name
-      web_image_tag = var.web_image_tag
+      web_image_tag   = var.web_image_tag
     })
   }
 }
